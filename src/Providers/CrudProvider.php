ec3c509cda90106467a50fd1b7bca3692cb382e7
Dmitry Yakovlev <kkaweler@gmail.com>
2019-12-24 18:52:46 +0300

    [FIX] fix provider

--- src/Providers/CrudProvider.php
+++ src/Providers/CrudProvider.php
@@ -26,60 +26,60 @@ class CrudProvider extends ServiceProvider
      * @var Form
      */
     private $form;
-
+    
     /**
      * @var CrudRoutesBinder
      */
     private $crudRoutesBinder;
-
+    
     /**
      * @var ModelFormDataSetter
      */
     private $modelFormDataSetter;
-
+    
     public function boot()
     {
         $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'simple_crud');
-
+        
         $configPath = __DIR__ . '/../../config/simple_crud.php';
-
+        
         if (function_exists('config_path')) {
             $publishPath = config_path('simple_crud.php');
         } else {
             $publishPath = base_path('config/simple_crud.php');
         }
-
+        
         $this->publishes([
             $configPath => $publishPath,
             __DIR__ . '/../../resources/views' => resource_path('views/vendor/simple_crud')
         ]);
     }
-
+    
     public function register()
     {
         $this->mergeConfigFrom(
             __DIR__ . '/../../config/simple_crud.php', 'simple_crud'
         );
-
+        
         $this->crudRoutesBinder = $this->app->make(CrudRoutesBinder::class);
         $this->form = new Form(config('simple_crud.form.banned_attributes'));
         $this->modelFormDataSetter = new  ModelFormDataSetter(
             config('simple_crud.form_data_setter.remove_spaces_keys'),
             config('simple_crud.form_data_setter.ignore_keys')
         );
-
+        
         $this->app->bind('crud_routes_binder', function () {
             return $this->crudRoutesBinder;
         });
-
+        
         $this->app->bind(Form::class, function () {
             return $this->form;
         });
-
+        
         $this->app->bind(ModelFormDataSetter::class, function () {
-            $this->modelFormDataSetter;
+            return $this->modelFormDataSetter;
         });
-
+        
         View::share('form', $this->app->make(Form::class));
     }
 }
