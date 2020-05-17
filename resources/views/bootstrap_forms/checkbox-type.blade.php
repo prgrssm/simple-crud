7cc8439fe6a3b95a8400225828b60cb3c38c47af
Dmitry Yakovlev <dima.j@symfomyart.com>
2020-05-17 20:14:48 +0300

    [FAETURE] improved checkbox

--- resources/views/bootstrap_forms/checkbox-type.blade.php
+++ resources/views/bootstrap_forms/checkbox-type.blade.php
@@ -1,7 +1,8 @@
 <div class="form-group">
   <div class="checkbox">
     <label>
-      <input type="checkbox" name="{{$name}}" @if($value)checked="checked"@endif>
+      <input type="hidden" name="{{$name}}" value="0">
+      <input type="checkbox" name="{{$name}}" @if($value)checked="checked" @endif value="1">
       {{$label}}
     </label>
   </div>
